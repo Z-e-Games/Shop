9bc7aa9fb2de0f87989db56f133c16fa713de8fd
Z-e-Games <93876011+Z-e-Games@users.noreply.github.com>
2021-11-14 18:27:45 -0600

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,23 +1,23 @@
 function makePwrUpsLastOneSecLonger(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 150 && localStorage.getItem('is signed in') === 'yes'){
-        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-150)
+    if(parseInt(localStorage.getItem('gold coins')) >= 500 && localStorage.getItem('is signed in') === 'yes'){
+        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-500)
         localStorage.setItem('ms per pwr up',parseInt(localStorage.getItem('ms per pwr up') ) + 1000)
         alert(`your powerups now last ${parseInt(localStorage.getItem('ms per pwr up'))/1000} seconds`)
     }else if(localStorage.getItem('is signed in') === 'no'){
         alert('please sign in first')
-    }else if(parseInt(localStorage.getItem('gold coins')) <= 150 && localStorage.getItem('is signed in') === 'yes'){
+    }else if(parseInt(localStorage.getItem('gold coins')) <= 500 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
     }
 }
 
 function setSGPSBMasDefault(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 300 && localStorage.getItem('is signed in') === 'yes'){
-        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))- 300)
+    if(parseInt(localStorage.getItem('gold coins')) >= 600 && localStorage.getItem('is signed in') === 'yes'){
+        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))- 600)
         localStorage.setItem('hasSquidGameMusic','yes')
         alert(`Your current music choice is Squid Game Pink Soldiers`)
     }else if(localStorage.getItem('is signed in') === 'no'){
         alert('please sign in first')
-    }else if(parseInt(localStorage.getItem('gold coins')) <= 300 && localStorage.getItem('is signed in') === 'yes'){
+    }else if(parseInt(localStorage.getItem('gold coins')) <= 600 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
     }
 }
