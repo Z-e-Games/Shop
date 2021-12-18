127c395f4cd4139b823dcf7b3854024200f9cf64
Z-e-Games <93876011+Z-e-Games@users.noreply.github.com>
2021-12-17 22:10:11 -0600

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,35 +1,35 @@
 function makePwrUpsLastOneSecLonger(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 500 && localStorage.getItem('is signed in') === 'yes'){
-        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-500)
+    if(parseInt(localStorage.getItem('gold coins')) >= 1000 && localStorage.getItem('is signed in') === 'yes'){
+        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-1000)
         localStorage.setItem('ms per pwr up',parseInt(localStorage.getItem('ms per pwr up') ) + 1000)
         alert(`your powerups now last ${parseInt(localStorage.getItem('ms per pwr up'))/1000} seconds`)
     }else if(localStorage.getItem('is signed in') === 'no'){
         alert('please sign in first')
-    }else if(parseInt(localStorage.getItem('gold coins')) <= 500 && localStorage.getItem('is signed in') === 'yes'){
+    }else if(parseInt(localStorage.getItem('gold coins')) <= 1000 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
     }
 }
 
 function setSGPSBMasDefault(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 600 && localStorage.getItem('is signed in') === 'yes'){
-        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))- 600)
+    if(parseInt(localStorage.getItem('gold coins')) >= 1200 && localStorage.getItem('is signed in') === 'yes'){
+        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))- 1200)
         localStorage.setItem('hasSquidGameMusic','yes')
         alert(`Your current music choice is Squid Game Pink Soldiers`)
     }else if(localStorage.getItem('is signed in') === 'no'){
         alert('please sign in first')
-    }else if(parseInt(localStorage.getItem('gold coins')) <= 600 && localStorage.getItem('is signed in') === 'yes'){
+    }else if(parseInt(localStorage.getItem('gold coins')) <= 1200 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
     }
 }
 
 function moneyPerKillUpgrade(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 3500 && localStorage.getItem('is signed in') === 'yes'){
-        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-3500)
+    if(parseInt(localStorage.getItem('gold coins')) >= 6500 && localStorage.getItem('is signed in') === 'yes'){
+        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-6500)
         localStorage.setItem('coins per kill',parseInt(localStorage.getItem('coins per kill') ) + 1)
-        alert(`You now get ${parseInt(localStorage.getItem('coins per kill'))} soins per kill.`)
+        alert(`You now get ${parseInt(localStorage.getItem('coins per kill'))} coins per kill.`)
     }else if(localStorage.getItem('is signed in') === 'no'){
         alert('please sign in first')
-    }else if(parseInt(localStorage.getItem('gold coins')) <= 500 && localStorage.getItem('is signed in') === 'yes'){
+    }else if(parseInt(localStorage.getItem('gold coins')) <= 6500 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
     }
 }
