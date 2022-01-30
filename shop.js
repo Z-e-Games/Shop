a15e168d7d5764f54838e1ae25cc94c5e1436b7a
Z-e-Games <93876011+Z-e-Games@users.noreply.github.com>
2022-01-29 21:08:58 -0600

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,5 +1,5 @@
 function makePwrUpsLastOneSecLonger(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 1000 && localStorage.getItem('is signed in') === 'yes'){
+    if(parseInt(localStorage.getItem('gold coins')) >= 1000 && localStorage.getItem('is signed in') === 'yes' && parseInt(localStorage.getItem('ms per pwr up')) < 30){
         localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-1000)
         localStorage.setItem('ms per pwr up',parseInt(localStorage.getItem('ms per pwr up') ) + 1000)
         alert(`your powerups now last ${parseInt(localStorage.getItem('ms per pwr up'))/1000} seconds`)
@@ -7,8 +7,13 @@ function makePwrUpsLastOneSecLonger(){
         alert('please sign in first')
     }else if(parseInt(localStorage.getItem('gold coins')) <= 1000 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
+    }else if(parseInt(localStorage.getItem('ms per pwr up')) > 30){
+        localStorage.setItem('ms per pwr up', 30)
+    }else if(parseInt(localStorage.getItem('ms per pwr up')) = 30){
+        alert('You have maxed out on this upgrade')
     }
 }
+
 
 function setSGPSBMasDefault(){
     if(parseInt(localStorage.getItem('gold coins')) >= 1200 && localStorage.getItem('is signed in') === 'yes'){
@@ -23,7 +28,7 @@ function setSGPSBMasDefault(){
 }
 
 function moneyPerKillUpgrade(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 6500 && localStorage.getItem('is signed in') === 'yes'){
+    if(parseInt(localStorage.getItem('gold coins')) >= 6500 && localStorage.getItem('is signed in') === 'yes' && parseInt(localStorage.getItem('coins per kill')) < 5){
         localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins'))-6500)
         localStorage.setItem('coins per kill',parseInt(localStorage.getItem('coins per kill') ) + 1)
         alert(`You now get ${parseInt(localStorage.getItem('coins per kill'))} coins per kill.`)
@@ -31,17 +36,21 @@ function moneyPerKillUpgrade(){
         alert('please sign in first')
     }else if(parseInt(localStorage.getItem('gold coins')) <= 6500 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
+    }else if(parseInt(localStorage.getItem('coins per kill')) > 5){
+        localStorage.setItem('coins per kill', 5)
+    }else if(parseInt(localStorage.getItem('coins per kill')) = 5){
+        alert('You have maxed out on this upgrade')
     }
 }
 
 function useElfSkin(){
-    if(parseInt(localStorage.getItem('gold coins')) >= 200 && localStorage.getItem('is signed in') === 'yes'){
-        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins')) - 200)
+    if(parseInt(localStorage.getItem('gold coins')) >= 400 && localStorage.getItem('is signed in') === 'yes'){
+        localStorage.setItem('gold coins',parseInt(localStorage.getItem('gold coins')) - 400)
         localStorage.setItem('has elf skin', 'yes')
         alert(`You will have the elf skin in the next game.`)
     }else if(localStorage.getItem('is signed in') === 'no'){
         alert('please sign in first')
-    }else if(parseInt(localStorage.getItem('gold coins')) <= 200 && localStorage.getItem('is signed in') === 'yes'){
+    }else if(parseInt(localStorage.getItem('gold coins')) <= 400 && localStorage.getItem('is signed in') === 'yes'){
         alert('sorry you dont have enough coins')
     }
 }
